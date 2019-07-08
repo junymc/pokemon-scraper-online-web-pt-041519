c3ac09aca99230404a33827bf6c810c60bea32ad
Junyoung McArdle <juny.mcardle@gmail.com>
2019-07-08 03:24:09 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,7 @@ class Pokemon
   attr_accessor :name, :type, :db, :hp
   attr_reader :id
 
-  def initialize(id:, name:, type:, db:)
+  def initialize(keywords)
     @id = id
     @name = name
     @type = type
